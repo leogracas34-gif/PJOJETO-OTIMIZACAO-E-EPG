5996044cc336c4064521b2776a524dee71bd9ff0
leogracas34-gif <leogracas34@gmail.com>
2025-12-25 09:23:24 -0300

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,14 +44,14 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    // Player de Vídeo (CORRIGIDO AQUI)
+    // Player de Vídeo
     implementation("androidx.media3:media3-exoplayer:1.2.0")
     implementation("androidx.media3:media3-ui:1.2.0")
     implementation("androidx.media3:media3-exoplayer-hls:1.2.0") 
     implementation("androidx.media3:media3-common:1.2.0")
 
-     // ✅ EPG + OTIMIZAÇÕES
-    implementation 'androidx.cardview:cardview:1.0.0'
-    implementation 'com.github.bumptech.glide:glide:4.16.0'
-    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1'
+    // ✅ EPG + OTIMIZAÇÕES (CORRIGIDO Kotlin DSL)
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
